dc8a5a0850cc3cd83f8850b2e237d628da5ff882
AMINE ELKARTITE <aminezth022@gmail.com>
2026-09-14 17:20:57 +0100

    feat: connect clients tab to CRUD manager

---
+++ mobile-admin/app/(tabs)/clients.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import {ResourceList} from '../../src/features/resources/ResourceList';
+export default function ClientsScreen(){return <ResourceList resource="clients"/>;}
